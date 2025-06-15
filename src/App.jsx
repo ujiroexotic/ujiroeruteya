45b945a9deb6f143647a164f6a8e6c20ff2728ca
Ujiro Eruteya <ujiroeruteya@gmail.com>
2025-06-15 02:21:12 +0100

    ujiroeruteya

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import SideNav from './components/SideNav';
+import SideNav from './components/Sidenav';
 import Main from './components/Main';
 import Work from './components/Work';
 import Projects from './components/Projects';
